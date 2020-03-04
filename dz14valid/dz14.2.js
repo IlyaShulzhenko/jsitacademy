8e4215a7b56c5e7e62c2ef66963a68e2d2a939ca
IlyaShulzhenko <60271555+IlyaShulzhenko@users.noreply.github.com>
2020-03-04 19:38:31 +0300

    Update dz14.2.js

--- dz14valid/dz14.2.js
+++ dz14valid/dz14.2.js
@@ -190,6 +190,12 @@ let formDef1 = [
     });
  });
 
+document.addEventListener('submit', function (event){
+	event.preventDefault();
+
+	removeErrors();
+   	validate();
+});
 
 function validate(item){
     if(item.value ===''){
