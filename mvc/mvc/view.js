bfa0d70dae5ad00739f3794598ddf2de67faea25
IlyaShulzhenko <60271555+IlyaShulzhenko@users.noreply.github.com>
2020-03-09 11:30:44 +0300

    Update view.js

--- mvc/mvc/view.js
+++ mvc/mvc/view.js
@@ -112,35 +112,34 @@ class View {
 
     }
     svgClock(){
-        const clockSvg = document.getElementById('svg');
-        
-        const xmls = 'http://www.w3.org/2000/svg';
-        const width = parseFloat(clockSvg.getAttributeNS(null, 'width'));
-        const height = parseFloat(clockSvg.getAttributeNS(null, 'height'));
-        const radiusClock = width / 2;
-        const radiusNumber = 0.75 * radiusClock;
-        
-        let widthHalf = width / 2;
-        let heightHalf = height / 2;
-        
-        
-        
-            let clockStyle = document.createElementNS(xmls, 'circle');
-        
-            clockStyle.setAttributeNS(null, 'cx', widthHalf);
-            clockStyle.setAttributeNS(null, 'cy', heightHalf);
-            clockStyle.setAttributeNS(null, 'r', widthHalf);
-            clockStyle.setAttributeNS(null, 'fill', 'yellow');
-            clockStyle.setAttributeNS(null, 'stroke', 'none');
-        
-            clockSvg.appendChild(clockStyle);
-        
-        
-        
-            
-            const bias = 30; 
-        
-            for (let i = 1; i <= 12; i++) {
+	const clockSvg = document.getElementById('svg');
+
+	const xmls = 'http://www.w3.org/2000/svg';
+	const width = parseFloat(clockSvg.getAttributeNS(null, 'width'));
+	const height = parseFloat(clockSvg.getAttributeNS(null, 'height'));
+	const radiusClock = width / 2;
+	const radiusNumber = 0.75 * radiusClock;
+
+	let widthHalf = width / 2;
+	let heightHalf = height / 2;
+
+
+	let clockStyle = document.createElementNS(xmls, 'circle');
+
+	clockStyle.setAttributeNS(null, 'cx', widthHalf);
+	clockStyle.setAttributeNS(null, 'cy', heightHalf);
+	clockStyle.setAttributeNS(null, 'r', widthHalf);
+	clockStyle.setAttributeNS(null, 'fill', 'yellow');
+	clockStyle.setAttributeNS(null, 'stroke', 'none');
+
+	clockSvg.appendChild(clockStyle);
+
+
+
+
+	const bias = 30; 
+
+        for (let i = 1; i <= 12; i++) {
         
                 let hourCircle = document.createElementNS(xmls, 'circle');
                 clockSvg.appendChild(hourCircle);
@@ -167,83 +166,78 @@ class View {
                 text.style.fontSize = '40';
                 text.style.fontWeight = 'bold';
                 text.style.textAnchor = 'middle';
-	    	svgRun()
-            }
-        
-        
-        
-        
-            arrowHour = document.createElementNS(xmls, 'line');
-        
-            arrowHour.setAttributeNS(null, 'x1', widthHalf);
-            arrowHour.setAttributeNS(null, 'x2', widthHalf);
-            arrowHour.setAttributeNS(null, 'y1', widthHalf);
-            arrowHour.setAttributeNS(null, 'y2', 100);
-            arrowHour.setAttributeNS(null, 'stroke', 'black');
-            arrowHour.setAttributeNS(null, 'stroke-linecap', 'round');
-            arrowHour.setAttributeNS(null, 'stroke-width', 6);
-            arrowHour.setAttributeNS(null, 'id', 'hours');
-        
-            clockSvg.appendChild(arrowHour);
-        
-            arrowMinute = document.createElementNS(xmls, 'line');
-         
-            arrowMinute.setAttributeNS(null, 'x1', widthHalf);
-            arrowMinute.setAttributeNS(null, 'x2', widthHalf);
-            arrowMinute.setAttributeNS(null, 'y1', widthHalf);
-            arrowMinute.setAttributeNS(null, 'y2', 60);
-            arrowMinute.setAttributeNS(null, 'stroke', 'red');
-            arrowMinute.setAttributeNS(null, 'stroke-linecap', 'round');
-            arrowMinute.setAttributeNS(null, 'stroke-width', 4);
-            arrowMinute.setAttributeNS(null, 'id', 'minutes');
-         
-            clockSvg.appendChild(arrowMinute);
-        
-            arrowSecond = document.createElementNS(xmls, 'line');
-            
-            arrowSecond.setAttributeNS(null, 'x1', widthHalf);
-            arrowSecond.setAttributeNS(null, 'x2', widthHalf);
-            arrowSecond.setAttributeNS(null, 'y1', widthHalf);
-            arrowSecond.setAttributeNS(null, 'y2', 40);
-            arrowSecond.setAttributeNS(null, 'stroke', 'white');
-            arrowSecond.setAttributeNS(null, 'stroke-linecap', 'round');
-            arrowSecond.setAttributeNS(null, 'stroke-width', 2);
-            arrowSecond.setAttributeNS(null, 'id', 'seconds');
-            
-            clockSvg.appendChild(arrowSecond);
-        
-        
-        const digitalСlock = document.createElementNS(xmls, 'text');
-        clockSvg.appendChild(digitalСlock);
-        
-        digitalСlock.setAttributeNS(null, 'x', 300);
-        digitalСlock.setAttributeNS(null, 'y', 200);
-        digitalСlock.setAttributeNS(null, 'id', 'digital-clock');
-        digitalСlock.style.fontSize = '2rem';
-        digitalСlock.style.fontWeight = 'bold';
-        digitalСlock.style.textAnchor = 'middle';
+	    	
+            }      
+                
+	arrowHour = document.createElementNS(xmls, 'line');
+
+	arrowHour.setAttributeNS(null, 'x1', widthHalf);
+	arrowHour.setAttributeNS(null, 'x2', widthHalf);
+	arrowHour.setAttributeNS(null, 'y1', widthHalf);
+	arrowHour.setAttributeNS(null, 'y2', 100);
+	arrowHour.setAttributeNS(null, 'stroke', 'black');
+	arrowHour.setAttributeNS(null, 'stroke-linecap', 'round');
+	arrowHour.setAttributeNS(null, 'stroke-width', 6);
+	arrowHour.setAttributeNS(null, 'id', 'hours');
+
+	clockSvg.appendChild(arrowHour);
+
+	arrowMinute = document.createElementNS(xmls, 'line');
+
+	arrowMinute.setAttributeNS(null, 'x1', widthHalf);
+	arrowMinute.setAttributeNS(null, 'x2', widthHalf);
+	arrowMinute.setAttributeNS(null, 'y1', widthHalf);
+	arrowMinute.setAttributeNS(null, 'y2', 60);
+	arrowMinute.setAttributeNS(null, 'stroke', 'red');
+	arrowMinute.setAttributeNS(null, 'stroke-linecap', 'round');
+	arrowMinute.setAttributeNS(null, 'stroke-width', 4);
+	arrowMinute.setAttributeNS(null, 'id', 'minutes');
+
+	clockSvg.appendChild(arrowMinute);
+
+	arrowSecond = document.createElementNS(xmls, 'line');
+
+	arrowSecond.setAttributeNS(null, 'x1', widthHalf);
+	arrowSecond.setAttributeNS(null, 'x2', widthHalf);
+	arrowSecond.setAttributeNS(null, 'y1', widthHalf);
+	arrowSecond.setAttributeNS(null, 'y2', 40);
+	arrowSecond.setAttributeNS(null, 'stroke', 'white');
+	arrowSecond.setAttributeNS(null, 'stroke-linecap', 'round');
+	arrowSecond.setAttributeNS(null, 'stroke-width', 2);
+	arrowSecond.setAttributeNS(null, 'id', 'seconds');
+
+	clockSvg.appendChild(arrowSecond);
+
+
+	const digitalСlock = document.createElementNS(xmls, 'text');
+	clockSvg.appendChild(digitalСlock);
+
+	digitalСlock.setAttributeNS(null, 'x', 300);
+	digitalСlock.setAttributeNS(null, 'y', 200);
+	digitalСlock.setAttributeNS(null, 'id', 'digital-clock');
+	digitalСlock.style.fontSize = '2rem';
+	digitalСlock.style.fontWeight = 'bold';
+	digitalСlock.style.textAnchor = 'middle';
     }
 	svgRun(){    
-                   
-            let seconds = s * 6;
-            let minutes = m * 6;
-            let hours = (h + m / 60 + s * 3600) * 30;
-    
-            let hoursStyle = document.getElementById('hours');
-            let minutesStyle = document.getElementById('minutes');
-            let secondsStyle = document.getElementById('seconds');
-            
-            secondsStyle.setAttributeNS(null, 'transform', 'rotate(' + seconds + ' 300 300)');
-            minutesStyle.setAttributeNS(null, 'transform', 'rotate(' + minutes + ' 300 300)');
-            hoursStyle.setAttributeNS(null, 'transform', 'rotate(' + hours + ' 300 300)');
-            
-            
-               
-                const digitalHours = (h < 10) ? ('0' + h) : (h);
-                const digitalMinutes = (m < 10) ? ('0' + m) : (m);
-                const digitalSeconds = (s < 10) ? ('0' + s) : (s);
-    
-                document.getElementById("digital-clock").textContent = `${digitalHours} : ${digitalMinutes} : ${digitalSeconds}`;
+
+		let seconds = s * 6;
+		let minutes = m * 6;
+		let hours = (h + m / 60 + s * 3600) * 30;
+
+		let hoursStyle = document.getElementById('hours');
+		let minutesStyle = document.getElementById('minutes');
+		let secondsStyle = document.getElementById('seconds');
+
+		secondsStyle.setAttributeNS(null, 'transform', 'rotate(' + seconds + ' 300 300)');
+		minutesStyle.setAttributeNS(null, 'transform', 'rotate(' + minutes + ' 300 300)');
+		hoursStyle.setAttributeNS(null, 'transform', 'rotate(' + hours + ' 300 300)');
+
+		const digitalHours = (h < 10) ? ('0' + h) : (h);
+		const digitalMinutes = (m < 10) ? ('0' + m) : (m);
+		const digitalSeconds = (s < 10) ? ('0' + s) : (s);
+
+		document.getElementById("digital-clock").textContent = `${digitalHours} : ${digitalMinutes} : ${digitalSeconds}`;
         }    
    }
        
