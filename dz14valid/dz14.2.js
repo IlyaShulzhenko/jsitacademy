3a0dc27af197da8f92fe7820406f014833e5039e
IlyaShulzhenko <60271555+IlyaShulzhenko@users.noreply.github.com>
2020-04-06 21:33:13 +0300

    Update dz14.2.js

--- dz14valid/dz14.2.js
+++ dz14valid/dz14.2.js
@@ -1,231 +1,253 @@
 let formDef1 = [
-    { label: 'Название сайта:', kind: 'longtext', name: 'sitename' },
-    { label: 'URL сайта:', kind: 'longtext', name: 'siteurl' },
-    { label: 'Посетителей в сутки:', kind: 'number', name: 'visitors' },
-    { label: "E-mail для связи:", kind: "shorttext", name: "email" },
-    {
-      label: 'Рубрика каталога:',
-      kind: 'kombo',
-      name: 'division',
-      variants: [
-        { text: 'здоровье', value: 1 },
-        { text: 'домашний уют', value: 2 },
-        { text: 'бытовая техника', value: 3 }
-      ]
-    },
-    {
-      label: 'Размещение:',
-      kind: 'radio',
-      name: 'payment',
-      variants: [
-        { text: 'бесплатное', value: 1 },
-        { text: 'платное', value: 2 },
-        { text: 'VIP', value: 3 }
-      ]
-    },
-    { label: 'Разрешить отзывы:', kind: 'check', name: 'votes' },
-    { label: 'Описание сайта:', kind: 'memo', name: 'description' },
-    { label: 'Опубликовать:', kind: 'submit' }
-  ];
+  { label: 'Название сайта:', kind: 'longtext', name: 'sitename' },
+  { label: 'URL сайта:', kind: 'longtext', name: 'siteurl' },
+  { label: 'Посетителей в сутки:', kind: 'number', name: 'visitors' },
+  { label: "E-mail для связи:", kind: "shorttext", name: "email" },
+  {
+    label: 'Рубрика каталога:',
+    kind: 'kombo',
+    name: 'division',
+    variants: [
+      { text: 'здоровье', value: 1 },
+      { text: 'домашний уют', value: 2 },
+      { text: 'бытовая техника', value: 3 }
+    ]
+  },
+  {
+    label: 'Размещение:',
+    kind: 'radio',
+    name: 'payment',
+    variants: [
+      { text: 'бесплатное', value: 1 },
+      { text: 'платное', value: 2 },
+      { text: 'VIP', value: 3 }
+    ]
+  },
+  { label: 'Разрешить отзывы:', kind: 'check', name: 'votes' },
+  { label: 'Описание сайта:', kind: 'memo', name: 'description' },
+  { label: 'Опубликовать:', kind: 'submit' }
+];
 
-  let formDef2 = [
-    { label: 'Фамилия:', kind: 'longtext', name: 'lastname' },
-    { label: 'Имя:', kind: 'longtext', name: 'firstname' },
-    { label: 'Отчество:', kind: 'longtext', name: 'secondname' },
-    { label: 'Возраст:', kind: 'number', name: 'age' },
-    { label: 'Зарегистрироваться:', kind: 'submit' }
-  ];
+let formDef2 = [
+  { label: 'Фамилия:', kind: 'longtext', name: 'lastname' },
+  { label: 'Имя:', kind: 'longtext', name: 'firstname' },
+  { label: 'Отчество:', kind: 'longtext', name: 'secondname' },
+  { label: 'Возраст:', kind: 'number', name: 'age' },
+  { label: 'Зарегистрироваться:', kind: 'submit' }
+];
 
-  let form1 = document.createElement('form');
-  form1.setAttribute('action', 'http://fe.it-academy.by/TestForm.php');
+let form1 = document.createElement('form');
+form1.setAttribute('action', 'http://fe.it-academy.by/TestForm.php');
 
-  let form2 = document.createElement('form');
-  form2.setAttribute('action', 'http://fe.it-academy.by/TestForm.php');
+let form2 = document.createElement('form');
+form2.setAttribute('action', 'http://fe.it-academy.by/TestForm.php');
 
-  document.body.appendChild(render(form1, formDef1));
-  document.body.appendChild(render(form2, formDef2));
+document.body.appendChild(render(form1, formDef1));
+document.body.appendChild(render(form2, formDef2));
 
-  function render(parentElment, elementObject) {
-        let output = document.createElement('div');
+function render(parentElment, elementObject) {
+      let output = document.createElement('div');
 
-        elementObject.forEach(element => {
-          let newElement;
+      elementObject.forEach(element => {
+        let newElement;
 
-          if (element.kind === 'longtext' || element.kind === 'shorttext') {
-              const input = renderInput('text', element);
+        if (element.kind === 'longtext' || element.kind === 'shorttext') {
+            const input = renderInput('text', element);
 
-                newElement = renderLabel(element.name, element.label);
-                newElement.appendChild(input);
-          } else if (element.kind === 'number') {
-                const input = renderInput('number', element);
+              newElement = renderLabel(element.name, element.label);
+              newElement.appendChild(input);
+        } else if (element.kind === 'number') {
+              const input = renderInput('number', element);
 
-              	newElement = renderLabel(element.name, element.label);
-                newElement.appendChild(input);
-          } else if (element.kind === 'kombo') {
-                newElement = renderSelect(element);
-          } else if (element.kind === 'radio') {
-                newElement = renderRadio(element);
-          } else if (element.kind === 'check') {
-              	newElement = renderCheckobox(element);
-          } else if (element.kind === 'memo') {
-              	newElement = renderTextArea(element);
-          } else if (element.kind === 'submit') {
-              	newElement = renderSubmit(element);
-          }
-
-          parentElment.appendChild(newElement);
-        });
-
-        return output.appendChild(parentElment);
-  }
-
-  function renderLabel(parentElement, text) {
-        const label = document.createElement('label');
-
-        label.textContent = text;
-
-        if (parentElement) {
-          label.setAttribute('for', parentElement);
+            newElement = renderLabel(element.name, element.label);
+              newElement.appendChild(input);
+        } else if (element.kind === 'kombo') {
+              newElement = renderSelect(element);
+        } else if (element.kind === 'radio') {
+              newElement = renderRadio(element);
+        } else if (element.kind === 'check') {
+            newElement = renderCheckobox(element);
+        } else if (element.kind === 'memo') {
+            newElement = renderTextArea(element);
+        } else if (element.kind === 'submit') {
+            newElement = renderSubmit(element);
         }
 
-        return label;
-  }
+        parentElment.appendChild(newElement);
+      });
 
-  function renderInput(type, element) {
-      let input = document.createElement('input');
+      return output.appendChild(parentElment);
+}
 
-        input.setAttribute('type', type);
-      	input.setAttribute('name', element.name);
-
-      if (element.kind === 'shorttext') {
-      	input.setAttribute('maxlength', 30);
-      }
-
-      return input;
-  }
-
-  function renderSelect(element) {
-      const output = document.createElement('div');
-      const select = document.createElement('select');
-      const variants = element.variants;
+function renderLabel(parentElement, text) {
       const label = document.createElement('label');
 
-      label.textContent = element.label;
-      label.appendChild(select);
-      select.setAttribute('name', element.name)
-      output.textContent = element.designation;
+      label.textContent = text;
 
-        for (let i = 0; i < variants.length; i++) {
-          const option = document.createElement('option');
-
-          option.setAttribute('value', variants[i].value);
-          option.textContent = variants[i].text;
-
-          select.appendChild(option);
-        }
-
-        return output.appendChild(label);
-  }
-
-  function renderRadio(element) {
-      const output = document.createElement('div');
-      output.textContent = element.label;
-
-      for (let i = 0; i < element.variants.length; i++) {
-          const labelElement = renderLabel(element.variants[i].value,	element.variants[i].text);
-          const input = renderInput('radio', element.name);
-
-          input.checked = true;
-          input.id = element.variants[i].value;
-          input.setAttribute('name', 'placement');
-          output.appendChild(input);
-          output.appendChild(labelElement);
+      if (parentElement) {
+        label.setAttribute('for', parentElement);
       }
 
-      return output;
-  }
+      return label;
+}
 
-  function renderCheckobox(element) {
-      const labelElement = renderLabel(null, element.label);
+function renderInput(type, element) {
+    let input = document.createElement('input');
+
+      input.setAttribute('type', type);
+    input.setAttribute('name', element.name);
+
+    if (element.kind === 'shorttext') {
+    input.setAttribute('maxlength', 30);
+    }
+
+    return input;
+}
+
+function renderSelect(element) {
+    const output = document.createElement('div');
+    const select = document.createElement('select');
+    const variants = element.variants;
+    const label = document.createElement('label');
+
+    label.textContent = element.label;
+    label.appendChild(select);
+    select.setAttribute('name', element.name)
+    output.textContent = element.designation;
+
+      for (let i = 0; i < variants.length; i++) {
+        const option = document.createElement('option');
+
+        option.setAttribute('value', variants[i].value);
+        option.textContent = variants[i].text;
+
+        select.appendChild(option);
+      }
+
+      return output.appendChild(label);
+}
+
+function renderRadio(element) {
+      const output = document.createElement('div');
+    output.textContent = element.label;
+
+    for (let i = 0; i < element.variants.length; i++) {
+        const labelElement = renderLabel(element.variants[i].value,	element.variants[i].text);
+        const input = renderInput('radio', element.name);
+
+        input.id = element.variants[i].value;
+        input.setAttribute('name', element.index);
+        output.appendChild(input);
+        output.appendChild(labelElement);
+      }
+      return output;
+}
+
+function renderCheckobox(element) {
+    const labelElement = renderLabel(null, element.label);
       const input = renderInput('checkbox', element.name);
 
-      labelElement.appendChild(input);
+    labelElement.appendChild(input);
 
 
-      return labelElement;
-  }
+    return labelElement;
+}
 
-  function renderTextArea(elementObject) {
-        let output = document.createElement('div');
-        const label = renderLabel(elementObject.name, elementObject.label);
-      	const textArea = document.createElement('textArea');
+function renderTextArea(elementObject) {
+      let output = document.createElement('div');
+      const label = renderLabel(elementObject.name, elementObject.label);
+    const textArea = document.createElement('textArea');
 
-        textArea.setAttribute('name', elementObject.name);
-        output.appendChild(label);
-      	output.appendChild(textArea);
+      textArea.setAttribute('name', elementObject.name);
+      output.appendChild(label);
+    output.appendChild(textArea);
 
-        return output;
-  }
+      return output;
+}
 
-  function renderSubmit(elementObject) {
-      let submit = document.createElement('button');
+function renderSubmit(elementObject) {
+    let submit = document.createElement('button');
 
       submit.setAttribute('type', 'submit');
-      submit.textContent = elementObject.label;
+    submit.textContent = elementObject.label;
 
-      return submit;
-  }
+    return submit;
+}
 
- const sitename = document.getElementsByClassName('sitename').sitename;
-const siteurl = document.getElementsByClassName('siteurl').siteurl;
-const visitors = document.getElementsByClassName('visitors').visitors;
-const email = document.getElementsByClassName('email').email;
-const votes = document.getElementsByClassName('votes').votes;
-const textarea = document.getElementsByClassName('textarea').description;
-const button = document.getElementsByClassName('submit');
-const form = document.getElementsByClassName('form');
-const input = document.getElementsByClassName('inputField');
+const inputTexts = document.querySelectorAll('input[type = "text"]');
+const inputNumbers = document.querySelectorAll('input[type = "number"]');
+const inputRadios = document.querySelector('input[type = "radio"]');
+const checkboxs = document.querySelectorAll('input[type = "checkbox"]');
+const selects = document.querySelector('select');
+const textareas = document.querySelector('textarea');
 
-const arr =[sitename, siteurl, visitors, email, votes, textarea];
+let array = [...inputTexts, ...inputNumbers, inputRadios, ...checkboxs, selects,textareas];
 
-document.addEventListener('submit', function (event){
-	event.preventDefault();
-	removeErrors();
-	validation();
-})
+array.forEach(item => {
+  item.addEventListener('blur', () => {
 
-arr.forEach(item=>{
-	item.addEventListener('blur', function(){
-		removeErrors();
-		validation();
-	});
+      validate(item);
+  });
 });
 
-function removeErrors(){
-	let errors = [...document.getElementsByClassName('error-text')];
-	for(let i=0; i<errors.length;i++){
-		errors[i].remove();
-	}
-}
-function validation(){
+document.addEventListener('submit', function (event){
+event.preventDefault();
 
-	arr.forEach (item =>{		
-		if(!item.value){
-			setErrorFor(item);
-			const errorText = document.createElement('div');
-			errorText.innerHTML = "please fill";
-			errorText.className = 'error-text';
-			item.parentElement.appendChild(errorText);
-		}else{
-			setSuccessFor(item);
-		}
-	})
-	if (!isEmail(email.value)) {
-		setErrorFor(email);
-	}	
+removeErrors();
+validates();
+
+  });
+
+
+
+function validate(item){
+  if(item.value ===''){
+      setAside(item);
+      const errorText = document.createElement('div');
+      errorText.textContent = 'Введите верное значение';
+      errorText.className = 'error';
+      item.parentElement.appendChild(errorText);
+  }else{
+      if (item.nextElementSibling) {
+          removeErrors();
+      }
+      addCaption(item);
+  }
 }
-function setErrorFor(input) {
-	input.style.borderColor = '#FF5964';
+
+function validates(){
+
+  array.forEach (item =>{
+  
+  if(!item.value){
+    setAside(item);
+
+    const errorText = document.createElement('div');
+    errorText.textContent = 'Введите верное значение';
+    errorText.className = 'error';
+    item.parentElement.appendChild(errorText);
+
+  }else{
+    addCaption(item);
+  }
+})
+
+
 }
-function setSuccessFor(input) {
-	input.style.borderColor = '#9EE279';
+
+function removeErrors(){
+let errors = document.getElementsByClassName('error');
+
+for(let i=0; i<errors.length; i++){
+    errors[i].remove();
+}
+}
+
+function setAside(input) {
+  input.style.borderColor = 'red';
+
+}
+
+function addCaption(input) {
+  input.style.borderColor = 'green';
+}
